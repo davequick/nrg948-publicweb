df55674964c8d16ffa1b8e6b2a347232a93952fc
Dave Quick <dave@quicks.me>
2015-11-18 21:35:04 -0800

    checkpoint

---
+++ index.js
@@ -0,0 +1,18 @@
+// app.js
+var koa = require('koa');
+var app = koa();
+var common = require('koa-common');
+
+// enable logger middleware
+app.use(common.logger('dev'));
+
+// enable static middleware
+app.use(common.static(__dirname + '/static'));
+
+app.use(function *() {
+    this.body = "Hello World";
+});
+
+var server = app.listen(4000, function () {
+    console.log('Listening on port %d', server.address().port);
+});
